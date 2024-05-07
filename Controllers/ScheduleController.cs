0948a2f81c1a91a3540b663eb3a05ff95e5c7e34
usdissanayake@gmail.com <sanjeewa@DESKTOP-K360SP3>
2024-05-07 17:40:22 +0530

    5.40

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -1,5 +1,7 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 using WebApplication1.Models;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -35,6 +37,72 @@ namespace WebApplication1.Controllers
             return View("AddEdit",model);
         }
 
+       
+        [HttpPost]
+
+        public IActionResult UpdatePOST(Schedule model)
+        {
+          
+
+            ViewBag.action = "UpdatePOST";
+            
+            List<Employee> data1 = HttpContext.Session.GetComplexData<List<Employee>>("loggerUser");
+       
+
+
+            var data = _db.schedules.Where(x => x.ScheduleID == model.ScheduleID).Select(s => new Schedule
+            {
+                ScheduleID = s.ScheduleID,
+                StartDate = s.StartDate,
+                StartTime = s.StartTime,
+                action = s.action,
+                Date = s.Date,
+        employee=s.employee.Where(x=>x.Include(si => si.Slot)).to,
+                EmployeeID = s.EmployeeID,
+                EndDate = s.EndDate,
+                EndTime = s.EndTime,
+                Name = s.Name
+
+
+            }).AsNoTracking().FirstOrDefault();
+
+
+            _db.schedules.Attach(data);
+            data.employee.RemoveAll(x => x.EmployeeID != 0);
+            data.employee=data1;
+
+            ViewBag.action = data.action;
+       
+            _db.SaveChanges();
+
+            ViewBag.employees = _db.employees.AsNoTracking().ToList();
+
+            return View("AddEdit", data);
+        }
+        public IActionResult Update(int scheduleid)
+        {
+
+            ViewBag.action = "UpdatePOST";
+
+            ViewBag.employees = _db.employees.ToList();
+            var data = _db.schedules.Where(x => x.ScheduleID == scheduleid).Select(s=>new Schedule
+            {
+                ScheduleID=s.ScheduleID,
+                StartDate=s.StartDate,
+                StartTime=s.StartTime,
+                action=s.action,
+                Date=s.Date,
+                employee=s.employee,
+                EmployeeID=s.EmployeeID,
+                EndDate=s.EndDate,
+                EndTime=s.EndTime,
+                Name=s.Name
+            }).FirstOrDefault();
+            data.action = "UpdatePOST";
+            HttpContext.Session.SetComplexData("loggerUser", data.employee);
+            return View("AddEdit",data);
+        }
+
         [HttpPost]
         public IActionResult delEmp(Schedule model,int empid)
         {
@@ -42,6 +110,7 @@ namespace WebApplication1.Controllers
             List<Employee> data1 = HttpContext.Session.GetComplexData<List<Employee>>("loggerUser");
             data1.Remove( data1.Where(x => x.EmployeeID == empid).FirstOrDefault());
             model.employee = data1;
+            ViewBag.action = model.action;
             HttpContext.Session.SetComplexData("loggerUser", model.employee);
             return View("AddEdit",model);
         }
@@ -65,6 +134,7 @@ namespace WebApplication1.Controllers
         }
         public IActionResult Index()
         {
+            HttpContext.Session.SetComplexData("loggerUser", null);
             var data = _db.schedules.ToList();
             return View(data);
         }
