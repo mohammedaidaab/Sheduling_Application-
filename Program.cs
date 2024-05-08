e9d72c2a30a4cfaacdba1fcf4f85a6b6ff46d8fb
usdissanayake@gmail.com <sanjeewa@DESKTOP-K360SP3>
2024-05-08 16:16:38 +0530

    4.15

--- Program.cs
+++ Program.cs
@@ -91,6 +91,6 @@ app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Calender}/{action=Index}/{id?}");
 
 app.Run();
